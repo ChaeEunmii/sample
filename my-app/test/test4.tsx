3b67c8a9f8a7d7b0b6fb738a22764f8f34f3c4cb
eunmi <chae.em92@gmail.com>
2025-09-05 12:13:06 +0900

    fs

---
+++ my-app/test/test4.tsx
@@ -0,0 +1,122 @@
+import { useState, useEffect } from "react";
+import { TitleArea, ButtonArea, Button, TitleBar } from "@shared/ui";
+import { AgreementGroup } from "@widgets/form";
+import { AGREEMENTS1 } from "@views/policy/agreements-data";
+
+export default function JoinStep1Case1() {
+  // 전체동의
+  const [agreeStates, setAgreeStates] = useState<Record<string, boolean>>({});
+  const [disabledStates, setDisabledStates] = useState<Record<string, boolean>>(
+    {}
+  );
+
+  const ALL_AGREE_ID = AGREEMENTS1.id;
+  const HYUNDAI_IDS = [
+    "hyundai-1",
+    "hyundai-2",
+    "hyundai-3",
+    "hyundai-4",
+    "hyundai-5",
+  ];
+
+  //동의 상태 변경 핸들러
+  const handleCheckChange = (id: string, checked: boolean) => {
+    // 전체체크 시
+    if (id === ALL_AGREE_ID && !checked) {
+      console.log("이것은 체크안되엇을때");
+    }
+
+    // 부모 id를 기준으로 disabled 해제 처리
+    if (id === "terms-homepage") {
+      setDisabledStates((prev) => {
+        const next = { ...prev };
+        HYUNDAI_IDS.forEach((k) => (next[k] = !checked)); // 체크되면 활성(false), 해제면 비활성(true)
+        return next;
+      });
+    }
+    // 체크 상태 갱신
+    setAgreeStates((prev) => ({ ...prev, [id]: checked }));
+  };
+
+  // 동의항목 초기설정
+  useEffect(() => {
+    // 체크항목 초기설정
+    const defaultCheckedId = ["hpoint-email", "hpoint-tm"];
+    // 비활성화 항목 초기설정
+    const defaultDisabledIds = [
+      "hpoint-email",
+      "hpoint-tm",
+      "hpoint-dm",
+      "hyundai-1",
+      "hyundai-2",
+      "hyundai-3",
+      "hyundai-4",
+      "hyundai-5",
+    ];
+
+    // 초기 체크 상태 설정
+    setAgreeStates((prev) => {
+      const updated = { ...prev };
+      defaultCheckedId.forEach((id) => {
+        updated[id] = true;
+      });
+      return updated;
+    });
+
+    // 초기 disabled 상태 설정
+    setDisabledStates((prev) => {
+      const updated = { ...prev };
+      defaultDisabledIds.forEach((id) => {
+        updated[id] = true;
+      });
+      return updated;
+    });
+  }, []);
+
+  return (
+    <>
+      <TitleArea
+        title={
+          <>
+            회원가입 약관에
+            <br />
+            동의해 주세요
+          </>
+        }
+      />
+      <TitleBar type="docs" title="필수약관에 동의해주세요" />
+      <AgreementGroup
+        data={AGREEMENTS1}
+        checked={agreeStates[AGREEMENTS1.id]}
+        onChange={handleCheckChange}
+        allStates={agreeStates}
+        disabledStates={disabledStates}
+        linkedChecks={[
+          {
+            triggers: ["hpoint-sms", "hpoint-email", "hpoint-tm", "hpoint-dm"],
+            targets: ["hpoint-8"],
+          },
+          {
+            triggers: ["ncp-sms", "ncp-email"],
+            targets: ["ncp-5"],
+          },
+          {
+            triggers: [
+              "hyundai-sms",
+              "hyundai-email",
+              "hyundai-tm",
+              "hyundai-dm",
+            ],
+            targets: ["hyundai-5"],
+          },
+        ]}
+      />
+      <ButtonArea>
+        <Button size="large">취소</Button>
+        <Button size="large" variant="primary">
+          동의
+        </Button>
+      </ButtonArea>
+    </>
+  );
+}
